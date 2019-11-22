87f8bc13c280fd1822708dc815605ab1ed953b63
vozoul <vozoul@gmail.com>
2019-11-22 14:53:39 +0100

    fix bugs

--- client/src/main/java/com/advenced_java/client/controller/MainController.java
+++ client/src/main/java/com/advenced_java/client/controller/MainController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
